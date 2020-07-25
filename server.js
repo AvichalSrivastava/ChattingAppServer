f17f22b1e6059a4b3aa8eb9cf02ff0eb8ebe9bc8
avichal22 <srivastava.avichal@gmail.com>
2020-07-25 18:47:56 +0530

    index.html added

--- server.js
+++ server.js
@@ -4,13 +4,11 @@ const jwt = require('jsonwebtoken');
 const app = express();
 var server = http.createServer(app);
 var io = require("socket.io").listen(server);
-var PORT = process.env.PORT || 3001;
+var PORT = process.env.PORT || 3000;
 
-io.on("connection", socket => {
-  console.log("a user connected");
-  socket.on("chat message", msg => {
-    console.log(msg);
-    io.emit("chat message", msg);
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
   });
 });
 
@@ -18,6 +16,9 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 //------------APIs Starts-------------------------------
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 app.get('/port',(req,res)=>
 {
   var token = jwt.sign({port:PORT}, 'shhhhh');
